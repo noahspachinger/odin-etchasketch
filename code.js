07fcf45f53402313f56c63880121a04dd1c1731d
Noah Spachinger <noahspachinger@gmail.com>
2023-02-21 09:41:26 +0100

    Align the divs in a grid

--- code.js
+++ code.js
@@ -2,14 +2,21 @@
 document.getElementById("adjustButton").addEventListener('click', createGrid);
 
 function createGrid() {
-    let numberOfDivs = prompt("Enter a number between 1 and 100 to determine the number of squares per side");
+    //Get the input from the user
+    let divsPerSide = prompt("Enter a number between 1 and 100 to determine the number of squares per side");
+    let numberOfDivs = divsPerSide * divsPerSide;
 
+    //Create all the divs
     for (let i = 0; i < numberOfDivs; i++) {
         let base = document.getElementById("base");
 
         let square = document.createElement('div');
         square.classList.add('squares');
-
+        
         base.appendChild(square);
     }
+
+    //Align the divs in the grid
+    document.getElementById("base").style.gridTemplateColumns = "repeat(" + divsPerSide + ", 1fr)";
+    document.getElementById("base").style.gridTemplateRows = "repeat(" + divsPerSide + ", 1fr)";
 }
