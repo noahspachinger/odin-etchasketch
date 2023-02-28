45f481d7f332e8c9cfd98c64acd03970cbdf0adc
Noah Spachinger <noahspachinger@gmail.com>
2023-02-28 17:35:26 +0100

    Change color from white to a random color

--- code.js
+++ code.js
@@ -41,5 +41,13 @@ function createGrid() {
 }
 
 function changeColor() {
-    this.style.background = "black";
+    this.style.background = randomColor();
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random()* 256);
+    let g = Math.floor(Math.random()* 256);
+    let b = Math.floor(Math.random()* 256);
+    let color = "rgb("+r+","+g+","+b+")";
+    return color;
 }
